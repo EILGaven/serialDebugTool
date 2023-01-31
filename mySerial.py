caa2e9eab51b5283532f22a9f8774e21bfecd5c9
Gaven <xiaov-wei@163.com>
2023-01-31 15:23:55 +0800

    修改了UI布局，自适应窗体大小

--- mySerial.py
+++ mySerial.py
@@ -4,23 +4,27 @@ import struct
 import sys
 import time
 
-from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow,QDialog,\
-    QMessageBox,QLabel,QPushButton,QPlainTextEdit,QTextEdit,QVBoxLayout,QHBoxLayout
-from PyQt5 import QtGui,QtCore
-from PyQt5.QtCore import QThread,pyqtSignal,qDebug,QSettings,QVariant,Qt,QObject,QPoint
-from PyQt5.QtGui import QIcon,QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDialog,\
+    QMessageBox, QLabel, QPushButton, QPlainTextEdit, QTextEdit, QVBoxLayout, QHBoxLayout
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import QThread, pyqtSignal, qDebug, QSettings, QVariant, Qt, QObject, QPoint
+from PyQt5.QtGui import QIcon, QPixmap
 #from qt_material import appl_stylesheet
 
 #import mySerialUI
-from mySerialUI import Ui_MainWindow
+# from mySerialUI import Ui_MainWindow
+from Ui_mySerialUI import Ui_MainWindow
 from serialUtils import get_ports, open_port
+
 
 class serialThread(QThread):
     data_arrivce_signal = pyqtSignal(name='serial_data')
-    def __init__(self,ser=None):
+
+    def __init__(self, ser=None):
         super().__init__()
         self.ser = ser
         self.data = ''
+
     def run(self):
         while True:
             if self.ser and self.ser.inWaiting():
@@ -41,12 +45,13 @@ class serialThread(QThread):
 #                     data.append(s)
 #                 print(data)
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        #mySerialUI.QtCore.seria
+        # mySerialUI.QtCore.seria
         self.initialize_ui()
         self.read_setting()
         self.currentPort = None
@@ -77,15 +82,17 @@ class MainWindow(QMainWindow):
 
     def read_setting(self):
         setting = QSettings('serial_config', 'serial_app')
-        #window
+        # window
         if setting.value('geometry') is not None:
             self.restoreState(setting.value('geometry'))
         if setting.value('windowState') is not None:
             self.restoreState(setting.value('windowState'))
         if setting.value('port_name') is not None:
-            self.ui.serialSelection.setCurrentIndex(int(setting.value('port_name')))
+            self.ui.serialSelection.setCurrentIndex(
+                int(setting.value('port_name')))
         if setting.value('baudrate') is not None:
-            self.ui.baudrateSelection.setCurrentIndex(int(setting.value('baudrate')))
+            self.ui.baudrateSelection.setCurrentIndex(
+                int(setting.value('baudrate')))
         if setting.value('byteSize') is not None:
             self.ui.byteSize.setCurrentIndex(int(setting.value('byteSize')))
         if setting.value('stopBit') is not None:
@@ -95,10 +102,10 @@ class MainWindow(QMainWindow):
 
     def save_setting(self):
         setting = QSettings('serial_config', 'serial_app')
-        #window
-        setting.setValue('geometry',self.saveGeometry())
-        setting.setValue('windowState',self.saveState())
-        setting.setValue('port_name',self.ui.serialSelection.currentIndex())
+        # window
+        setting.setValue('geometry', self.saveGeometry())
+        setting.setValue('windowState', self.saveState())
+        setting.setValue('port_name', self.ui.serialSelection.currentIndex())
         setting.setValue('baudrate', self.ui.baudrateSelection.currentIndex())
         setting.setValue('byteSize', self.ui.byteSize.currentIndex())
         setting.setValue('stopBit', self.ui.stopBit.currentIndex())
@@ -108,13 +115,13 @@ class MainWindow(QMainWindow):
         """:param data: str"""
         input_data = self.ui.inputData.toPlainText()
         print(input_data)
-        self.currentPort.write(input_data.encode('utf-8'))#('abc')
+        self.currentPort.write(input_data.encode('utf-8'))  # ('abc')
 
     def close_port(self):
         if self.currentPort is not None:
             self.serial_thread.ser = None
             self.currentPort.close()
-            #self.serial_thread.terminate()
+            # self.serial_thread.terminate()
             self.ui.portStatus.setText(self.currentPort.port + ' ' + 'Closed')
             self.ui.openPort.setEnabled(True)
             self.ui.closePort.setEnabled(False)
@@ -132,17 +139,19 @@ class MainWindow(QMainWindow):
             # portName, baudrate, byteSize, stopBit, parity
             currentPortName = self.ui.serialSelection.currentText()
             currentBaudrate = int(self.ui.baudrateSelection.currentText())
-            #print(self.ui.byteSize.currentText())
+            # print(self.ui.byteSize.currentText())
             currentByteSize = int(self.ui.byteSize.currentText())
-            print('currentByteSize:',currentByteSize,'type:',type(currentByteSize))
+            print('currentByteSize:', currentByteSize,
+                  'type:', type(currentByteSize))
             index = self.ui.stopBit.currentIndex()
             stopBitList = [1, 1.5, 2]
             currentStopBit = stopBitList[index]
             currentParity = self.ui.parity.currentText()[0]
             #print(currentByteSize, currentStopBit, currentParity)
-            self.currentPort = open_port(currentPortName,currentBaudrate, currentByteSize, currentStopBit, currentParity)
-            #print(self.currentPort.baudrate,type(self.currentPort.baudrate))
-            #self.currentPort.
+            self.currentPort = open_port(
+                currentPortName, currentBaudrate, currentByteSize, currentStopBit, currentParity)
+            # print(self.currentPort.baudrate,type(self.currentPort.baudrate))
+            # self.currentPort.
             if self.currentPort.isOpen():
                 self.serial_thread.ser = self.currentPort
                 self.ui.openPort.setEnabled(False)
@@ -156,19 +165,20 @@ class MainWindow(QMainWindow):
                 self.ui.sendData.setEnabled(False)
                 self.ui.portStatus.setText(currentPortName + ' ' + 'Closed')
 
-
     def initialize_ui(self):
         availablePorts = get_ports()
         for port in availablePorts:
             self.ui.serialSelection.addItem(port)
 
-        baudrateList = ['1200','2400','4800','9600','19200','38400','115200']
+        baudrateList = ['1200', '2400', '4800',
+                        '9600', '19200', '38400', '115200']
         for baudrate in baudrateList:
             self.ui.baudrateSelection.addItem(baudrate)
         self.ui.baudrateSelection.setCurrentText('9600')
 
+
 if __name__ == '__main__':
-    #get_ports()
+    # get_ports()
     app = QApplication(sys.argv)
     w = MainWindow()
     w.setWindowTitle("串口调试助手")
